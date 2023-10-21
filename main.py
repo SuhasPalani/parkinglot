cdee2fe051cc505157aaac27613e3896322938d9
SuhasPalani <sugipals007@gmail.com>
2023-10-21 17:53:29 -0500

    error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, flash
+from flask import Flask, redirect, render_template, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_manager, UserMixin,login_required,logout_user,login_user,LoginManager,current_user, user_accessed, user_logged_in
 import mysql.connector
@@ -9,7 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # my database connection
 local_server=True
 app=Flask(__name__)
-app.secret_key='parking_lot'
+app.secret_key='plms'
 
 
 # for getting unique user access
@@ -18,7 +18,7 @@ login_manager=LoginManager(app)
 login_manager.login_view='login'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://username:password@localhost/database name'
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:@localhost/parking_lot'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:Suhas@2324@localhost/parking_lot'
 db=SQLAlchemy(app)
 
 
@@ -38,10 +38,11 @@ class Customer(db.Model):
     fname=db.Column(db.String(20))
     lname=db.Column(db.String(20))
     email=db.Column(db.String(50))
-    phone=db.Column(db.String(50))
+    phone=db.Column(db.String(20))
     address=db.Column(db.String(50))
     uname=db.Column(db.String(20))
     password=db.Column(db.String(20))
+
 
 @app.route("/")
 def home():
@@ -57,23 +58,44 @@ def userlogin():
     return render_template("userlogin.html")
 
 
-@app.route('/signup', methods=['POST','GET'])
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
-        id=request.form.get('id')
-        fname=request.form.get('fname')
-        lname=request.form.get('lname')
-        email=request.form.get('email')
-        phone=request.form.get('phone')
-        address=request.form.get('Address')
-        uname=request.form.get('uname')
-        password=request.form.get('password')
-        # print(fname,lname,email,phone,Address,uname,password)
-        encpassword = generate_password_hash(password)
-        with db.engine.connect() as conn:
-            new_user = conn.execute(f"INSERT INTO `customer` (`cust_id `, `first_name`,`last_name`,`email`, `phone_number`, `address`, `user_name`,`password`) VALUES ('{id}', '{fname}','{lname}','{email}', '{phone}', '{address}', '{uname}','{encpassword}' )")
+        id = request.form.get('cid')
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        address = request.form.get('address')
+        uname = request.form.get('uname')
+        password = request.form.get('password')
         
-            return 'úser added'
+        # Check if the username or email already exists
+        existing_user = Customer.query.filter_by(user_name=uname).first()
+        existing_email = Customer.query.filter_by(email=email).first()
+        
+        if existing_user:
+            flash('Username already exists. Please choose another one.', 'error')
+        elif existing_email:
+            flash('Email address is already registered.', 'error')
+        else:
+            # Hash the password before storing it
+            encpassword = generate_password_hash(password)
+            new_user = Customer(
+                cust_id=id,
+                first_name=fname,
+                last_name=lname,
+                email=email,
+                phone_number=phone,
+                address=address,
+                user_name=uname,
+                password=encpassword
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User added successfully!', 'success')
+            return redirect(url_for('signup'))  # Redirect to the signup page
+
     return render_template("usersignup.html")
 
 
